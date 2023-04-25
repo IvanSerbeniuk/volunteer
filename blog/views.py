84af7d512af56fab97a57bd085f9d158a1d4b782
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-04-25 20:34:00 +0300

    blog page

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,8 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'blog.html'
+    template_name = 'blogusy.html'
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'single_blogus.html'
