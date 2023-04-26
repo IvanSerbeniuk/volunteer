15b0a337707cb9d00a31aa74f181995ae9906e4e
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-04-26 17:29:50 +0300

    img -

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,6 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    img
 
     class Meta:
         ordering = ['-created_on']
