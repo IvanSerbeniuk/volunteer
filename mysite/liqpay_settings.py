42da3ecb19a572437a6b84d5bb08f30d33b26a26
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-08-18 14:06:09 +0300

    zero donation1

---
+++ mysite/liqpay_settings.py
@@ -0,0 +1,5 @@
+from decouple import config
+
+LIQPAY_PUBLIC_KEY =  'sandbox_i97618994403' #config('YOUR_PUBLIC_KEY')
+LIQPAY_PRIVATE_KEY = 'sandbox_997Trh625acVmzO9c2Syd5EGA7D31eTQDgfYuufG' #config('YOUR_PRIVATE_KEY')
+LIQPAY_SANDBOX_MODE = True  # Установите в True для тестирования в режиме песочницы (sandbox mode)
