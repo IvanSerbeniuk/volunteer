00fc42099ec5f0fc762f481bdb1c112e7cd30dc3
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-08-19 21:33:19 +0300

    donation

--- home/views.py
+++ home/views.py
@@ -3,3 +3,10 @@ from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 
 
+def home(request):
+    return render(request, 'index.html')
+
+def successMsg(request, args):
+    amount = args
+    return render(request, 'templates/donation_success.html', {'amount': amount})
+
