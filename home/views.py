1159af75f3e04819ec496dea7d094c71082bc7d7
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-08-22 00:53:01 +0300

    3 Posts from blog on main pg

--- home/views.py
+++ home/views.py
@@ -4,11 +4,14 @@ from django.conf import settings
 
 from .models import Event
 from django.utils import timezone
+from blog.models import Post
 
 
 def home(request):
     events = Event.objects.filter(date__gte=timezone.now()).order_by('date')
-    return render(request, 'index.html', {'events': events})
+    # recent_posts = Post.objects.all()[:3]
+    recent_posts = Post.objects.all()[:3]
+    return render(request, 'index.html', {'events': events, 'recent_posts': recent_posts}, )#{'recent_posts': recent_posts}
 
 def successMsg(request, args):
     amount = args
