a0d278f0978cf9c2936b0df988a478f3b7d97e74
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-08-22 21:23:10 +0300

    recent posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('blog/', views.PostList.as_view(), name='blog'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('recent_posts/', views.RecentPosts.as_view(), name='recent_posts'), 
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,22 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blogusy.html'
+    context_object_name = 'posts'  # Имя переменной, которая будет доступна в шаблоне
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['recent_posts'] = Post.objects.filter(status=1).order_by('-created_on')[:5]
+    #     return context
 
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'single_blogus.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recent_posts'] = Post.objects.filter(status=1).order_by('-created_on')[:5]
+        return context
+
+class RecentPosts(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')[:5]
+    template_name = 'recent_posts.html'
+    context_object_name = 'recent_posts'
