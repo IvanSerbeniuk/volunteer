c6eefe24f4c0f13bcd9757a0a239238a955fe90c
DmytroSerbeniuk <dmytro.serbenyuk@1648factory.com>
2023-09-04 23:26:32 +0200

    Revert "add test commit"
    This reverts commit 0a312d581f4f46f9fcb1d22073fbf9a74a0e903d.

--- mysite/settings.py
+++ mysite/settings.py
@@ -98,8 +98,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
