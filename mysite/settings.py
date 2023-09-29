ef436fecbc12c33fb56d03c8487daa880f3582fb
DmytroSerbeniuk <dmytro.serbenyuk@1648factory.com>
2023-09-29 21:40:58 +0200

    env

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,8 +122,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('POSTGRES_DB','postgres'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'USER': config('POSTGRES_USER','postgres'),
+        'PASSWORD': config('POSTGRES_PASSWORD','postgres'),
         'HOST': config('POSTGRES_HOST'),
         'PORT': config('POSTGRES_PORT', 5432)
      }
