1e69dd714d0aea398a701d49b989d6ceb9f00fab
DmytroSerbeniuk <dmytro.serbenyuk@1648factory.com>
2023-09-29 21:51:27 +0200

    update os.getenv

--- mysite/settings.py
+++ mysite/settings.py
@@ -178,7 +178,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # aws settings
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 # s3 static settings
