6d2b1c6b2cfc1e29075f21581b4be92f62104dca
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-11-07 16:42:34 +0200

    IP address

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-h$q6n=l#p&5vt8$49i9&rma5(l3)h-86e*go!g1eficj-nt(c1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1','localhost','3.71.152.186','volunteer.dmytroserbeniuk.uk']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1','localhost','3.69.216.243','volunteer.dmytroserbeniuk.uk']
 
 
 # Application definition
@@ -113,35 +113,35 @@ DEBUG_TOOLBAR_PANELS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config('POSTGRES_DB','postgres'),
-#         'USER': config('POSTGRES_USER','postgres'),
-#         'PASSWORD': config('POSTGRES_PASSWORD','postgres'),
-#         'HOST': config('POSTGRES_HOST','localhost'),
-#         'PORT': config('POSTGRES_PORT', 5432)
-#      }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('POSTGRES_DB','postgres'),
+        'USER': config('POSTGRES_USER','postgres'),
+        'PASSWORD': config('POSTGRES_PASSWORD','postgres'),
+        'HOST': config('POSTGRES_HOST','localhost'),
+        'PORT': config('POSTGRES_PORT', 5432)
+     }
+}
 
 
-# REDIS_HOST = config('REDIS_HOST', '127.0.0.1')
-# REDIS_PORT = config('REDIS_PORT', '6379')
+REDIS_HOST = config('REDIS_HOST', '127.0.0.1')
+REDIS_PORT = config('REDIS_PORT', '6379')
 
-# ## Redis
+## Redis
 
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django_redis.cache.RedisCache",
-#         "LOCATION": f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
-#         "OPTIONS": {
-#             "db": 0,
-#             "parser_class": "redis.connection.PythonParser",
-#             "pool_class": "redis.BlockingConnectionPool",
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
+        "OPTIONS": {
+            "db": 0,
+            "parser_class": "redis.connection.PythonParser",
+            "pool_class": "redis.BlockingConnectionPool",
     
-#         }
-#     }
-# }
+        }
+    }
+}
 
 # Cache time to live is 15 minutes.
 CACHE_TTL = 60 * 15
