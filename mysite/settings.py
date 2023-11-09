345fa752ca08a4c323be81f30cbbeb339ffff881
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-11-09 21:21:35 +0200

    commented local db

--- mysite/settings.py
+++ mysite/settings.py
@@ -149,12 +149,12 @@ DEBUG_TOOLBAR_PANELS = [
 CACHE_TTL = 60 * 15
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 STATICFILES_DIRS = [BASE_DIR / "static"]  
 MEDIA_URL = '/media/'
