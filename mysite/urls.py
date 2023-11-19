b56487beeb250a6117d03452b86c99b86818b382
Dmitriy Serbeniuk <dserbenyukgood@gmail.com>
2023-11-19 14:50:31 +0100

    add swagger Respond Api

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django.conf import settings
 from django.conf.urls.static import static
-import debug_toolbar  
+import debug_toolbar
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Respond Api",
+        default_version='v1',),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    re_path(r'^doc(?P<format>\.json|\.yaml)$',schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),name='schema-redoc'), 
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('', include('blog.urls')),
